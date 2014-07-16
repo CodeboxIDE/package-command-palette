e5696221e2593a0d217cdb633474c4a00c6ec53c
Samy Pessé <samypesse@gmail.com>
2014-07-16 15:38:33 -0700

    Add base list for commands

--- src/views/commands.js
+++ src/views/commands.js
@@ -1,8 +1,15 @@
-define(function() {
+define([
+    "text!src/templates/command.html"
+], function(template) {
     var hr = codebox.require("hr/hr");
 
     var Command = hr.List.Item.extend({
-
+        template: template,
+        templateContext: function() {
+            return {
+                command: this.model
+            };
+        }
     });
 
     var Commands = hr.List.extend({
--- src/views/palette.js
+++ src/views/palette.js
@@ -3,9 +3,30 @@ define([
     "text!src/templates/palette.html"
 ], function(Commands, template) {
     var hr = codebox.require("hr/hr");
+    var commands = codebox.require("core/commands");
 
     var Palette = hr.View.extend({
-        template: template
+        template: template,
+
+        initialize: function() {
+            Palette.__super__.initialize.apply(this, arguments);
+
+            this.commands = new Commands({
+                collection: commands
+            });
+        },
+
+        render: function() {
+            this.commands.detach();
+
+            return Palette.__super__.render.apply(this, arguments);
+        },
+
+        finish: function() {
+            this.commands.$el.appendTo(this.$(".content"));
+
+            return Palette.__super__.finish.apply(this, arguments);
+        }
     });
 
     return Palette;
