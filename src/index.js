c13d861779fb300a6b949fdd2a052082200e3aab
Samy Pessé <samypesse@gmail.com>
2015-04-08 13:16:23 +0200

    Adapt build for browserify

--- src/index.js
+++ src/index.js
@@ -1,26 +1,24 @@
-define([
-    "text!src/templates/command.html"
-], function(commandTemplate) {
-    var commands = codebox.require("core/commands");
-    var dialogs = codebox.require("utils/dialogs");
+var commandTemplate = require("./templates/command.html");
 
-    commands.register({
-        id: "palette.open",
-        title: "Command Palette: Open",
-        shortcuts: [
-            "mod+shift+p"
-        ],
-        hidden: true,
-        run: function() {
-            return dialogs.list(commands, {
-                template: commandTemplate,
-                filter: function(command) {
-                    return command.get("hidden") !== true && command.isValidContext();
-                }
-            })
-            .then(function(command) {
-                return command.run();
-            });
-        }
-    });
+var commands = codebox.require("core/commands");
+var dialogs = codebox.require("utils/dialogs");
+
+commands.register({
+    id: "palette.open",
+    title: "Command Palette: Open",
+    shortcuts: [
+        "mod+shift+p"
+    ],
+    hidden: true,
+    run: function() {
+        return dialogs.list(commands, {
+            template: commandTemplate,
+            filter: function(command) {
+                return command.get("hidden") !== true && command.isValidContext();
+            }
+        })
+        .then(function(command) {
+            return command.run();
+        });
+    }
 });
